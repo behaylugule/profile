98eb645cc5df38c2360e9b687fbe7b98941600cc
behaylu <behaylugule@gmail.com>
2025-06-15 14:21:10 +0300

    fix image

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
-import { useTheme } from '@/context/ThemeContext';
+import { useEffect, useState } from 'react';
+// import { useTheme } from '@/context/ThemeContext';
 
 const ThemeToggle = () => {
-  const { isDarkMode, toggleDarkMode } = useTheme();
+  // const { isDarkMode, toggleDarkMode } = useTheme();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
